adc9436a5b04c3214b4a0ce25c384c5d0840b91e
Melinda Theresia <melindatheresia10@gmail.com>
2021-04-30 19:12:04 +0700

    whatsapp open link

--- LokasiVC.swift
+++ LokasiVC.swift
@@ -19,12 +19,32 @@ class LokasiVC: UIViewController {
 
         // Do any additional setup after loading the view.
      
-    
+        
         
         
     }
 
     @IBAction func kirimLokasiBtn(_ sender: Any) {
+        let phoneNumber =  "+6289619458979" // you need to change this number
+        
+        let longitude = 53.9187068 // coreLocation.getLongitude()
+        let latitude = 27.5862874 // coreLocation.getLatitude()
+        let locationUrl = "https://maps.google.com/?daddr=\(longitude),\(latitude)"
+    
+        
+        
+//        https://wa.me/08231231412?text=Halo%20name%20maya%20nadine
+        let appURL = URL(string: "https://wa.me/\(phoneNumber)?text=\(locationUrl)")!
+        if UIApplication.shared.canOpenURL(appURL) {
+            if #available(iOS 10.0, *) {
+                UIApplication.shared.open(appURL, options: [:], completionHandler: nil)
+            }
+            else {
+                UIApplication.shared.openURL(appURL)
+            }
+        } else {
+            // WhatsApp is not installed
+        }
     }
     
 }
