7d406ef186e41c725720b4cc292e06db399d8bdf
Melinda Theresia <melindatheresia10@gmail.com>
2021-04-30 17:20:59 +0700

    Storyboard kelar

---
+++ InfoSayaVC.swift
@@ -0,0 +1,29 @@
+//
+//  InfoSayaVC.swift
+//  AkuBisa
+//
+//  Created by Melinda Theresia on 28/04/21.
+//
+
+import UIKit
+
+class InfoSayaVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ LokasiVC.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  AkuBisa
+//
+//  Created by Melinda Theresia on 28/04/21.
+//
+
+import UIKit
+
+class LokasiVC: UIViewController {
+
+    @IBOutlet weak var judulAkuBisa: UILabel!
+    @IBOutlet weak var kirimLokasi: UIButton!
+    @IBOutlet weak var lokasiTabBar: UITabBarItem!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+     
+    
+        
+        
+    }
+
+    @IBAction func kirimLokasiBtn(_ sender: Any) {
+    }
+    
+}
+
