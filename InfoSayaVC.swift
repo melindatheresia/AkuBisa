11793e7a463fe78e871065788677655b52bcc6e0
Melinda Theresia <melindatheresia10@gmail.com>
2021-05-01 22:53:08 +0700

    Masukin label ke InfoSayaVC & bikin file EditInfoSayaVC

--- InfoSayaVC.swift
+++ InfoSayaVC.swift
@@ -10,38 +10,83 @@ import UIKit
 class InfoSayaVC: UIViewController {
 
     @IBOutlet weak var ubahInfo: UIButton!
-    @IBOutlet weak var photoProfile: UIImageView!
-    @IBOutlet weak var namaAnak: UILabel!
-    @IBOutlet weak var umurAnak: UILabel!
-    @IBOutlet weak var alamatAnak: UILabel!
-    @IBOutlet weak var namaKereta: UILabel!
-    @IBOutlet weak var kodeBooking: UILabel!
-    @IBOutlet weak var dariStasiun: UILabel!
-    @IBOutlet weak var keStasiun: UILabel!
-    @IBOutlet weak var jalurKereta: UILabel!
-    @IBOutlet weak var tempatDuduk: UILabel!
-    @IBOutlet weak var jamBerangkat: UILabel!
-    @IBOutlet weak var jamTiba: UILabel!
-    @IBOutlet weak var catatanDariOrtu: UILabel!
-    @IBOutlet weak var kontakSection: UILabel!
+    
+    @IBOutlet weak var namaAnakLabel: UILabel!
+    @IBOutlet weak var umurAnakLabel: UILabel!
+    @IBOutlet weak var alamatAnakLabel: UILabel!
+        
+    @IBOutlet weak var namaKeretaLabel: UILabel!
+    @IBOutlet weak var kodeBookingLabel: UILabel!
+        
+    @IBOutlet weak var dariStasiunLabel: UILabel!
+    @IBOutlet weak var keStasiunLabel: UILabel!
+    @IBOutlet weak var jalurKeretaLabel: UILabel!
+    @IBOutlet weak var tempatDudukLabel: UILabel!
+    @IBOutlet weak var jamBerangkatLabel: UILabel!
+    @IBOutlet weak var jamTibaLabel: UILabel!
+        
+    @IBOutlet weak var catatanOrtuLabel: UILabel!
+        
+    @IBOutlet weak var kontakLabel: UILabel!
     @IBOutlet weak var listKontak: UITableView!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        //        isiNama.delegate = self
+        //        isiNama.isHidden = true
+        //        namaAnak.isUserInteractionEnabled = true
+        //        let aSelector : Selector = Selector(("lblTapped"))
+        //        let tapGesture = UITapGestureRecognizer(target: self, action: aSelector)
+        //        tapGesture.numberOfTapsRequired = 1
+        //        namaAnak.addGestureRecognizer(tapGesture)
+                
+                saveDetails()
     }
-    
 
-    /*
-    // MARK: - Navigation
+        
+        func saveDetails() {
+//            setAllTextFieldHidden(isHidden: true)
+//            copyTextFieldKeLabel()
+            setAllLabelHidden(isHidden: false)
+        }
+        
+        func goToEditDetails() {
+            setAllLabelHidden(isHidden: true)
+//            copyLabelKeTextField()
+//            setAllTextFieldHidden(isHidden: false)
+        }
+        
+//        func copyLabelKeTextField() {
+//            namaKeretaTextField.text = namaKeretaLabel.text
+//        }
+//
+//        func copyTextFieldKeLabel() {
+//            namaKeretaLabel.text = namaKeretaTextField.text
+//        }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+        func setAllLabelHidden(isHidden: Bool) {
+            namaKeretaLabel.isHidden = isHidden
+        }
+        
+//        func setAllTextFieldHidden(isHidden: Bool) {
+//            namaKeretaTextField.isHidden = isHidden
+//        }
+        
+    //    func lblTapped(){
+    //        lbl.hidden = true
+    //        textF.hidden = false
+    //        textF.text = lbl.text
+    //    }
+    //
+    //    func textFieldShouldReturn(userText: UITextField) -> Bool {
+    //        userText.resignFirstResponder()
+    //        textF.hidden = true
+    //        lbl.hidden = false
+    //        lbl.text = textF.text
+    //        return true
+    //    }
+    //
 
 }
